4b654645907bdff142ab255e6edd2d45f2d739ee
zhch <zhchqingdao@163.com>
2011-11-06 23:35:52 +0800

    temp

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-java -cp bin;lib/commons-logging-1.0.4.jar;lib/log4j-1.2.16.jar;lib/ant-1.6.5.jar;lib/commons-io-1.4.jar zhch.illq.project.Creator
+java -cp target/classes;lib/commons-logging-1.0.4.jar;lib/log4j-1.2.16.jar;lib/ant-1.6.5.jar;lib/commons-io-1.4.jar zhch.illq.project.Creator
 pause
